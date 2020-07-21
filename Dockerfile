18f2d86772b44748d3f115fdb6beb1ffdce782ee
Robin Moser <mail@robinmoser.de>
2020-07-21 17:33:06 +0200

    correct webroot path

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM webdevops/php-nginx:7.4-alpine
 LABEL maintainer="mail@robinmoser.de"
 
-ENV WEB_ROOT="/app/src"
-
-COPY src /app/src
+COPY src /app
 COPY scripts/check-credentials.sh /entrypoint.d/check-credentials.sh
--- scripts/check-credentials.sh
+++ scripts/check-credentials.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ ! -f "$WEB_ROOT"/credentials.yml ]; then
+if [ ! -f "$WEB_DOCUMENT_ROOT"/credentials.yml ]; then
 
     echo "FATAL: Credentials File 'credentials.yml' not found!"
-    echo "Please create a credentials File and mount it into $WEB_ROOT for the application to work."
+    echo "Please create a credentials File and mount it into $WEB_DOCUMENT_ROOT for the application to work."
     echo "Exiting..."
 
     exit 1
